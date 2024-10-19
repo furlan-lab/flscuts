dc958ab3731da75e70898995ca941e70a360d998
Scott <scfurl@users.noreply.github.com>
2024-10-18 22:48:02 -0700

    flowplots

--- R/plotting.R
+++ R/plotting.R
@@ -178,3 +178,145 @@ plot_geneset <- function(cds,
     labs(color = loca) +
     scale_color_gradientn(colors = c("darkblue", "skyblue", "white", "red", "darkred"))
 }
+
+#' Generate a Dot Plot with ADT Data from a Seurat Object
+#'
+#' This function creates a dot plot using Antibody-Derived Tag (ADT) data from a Seurat object, applying log transformation and optional noise to the data. The function also provides customization options for colors, downsampling, and buffer adjustments for the plot axes.
+#'
+#' @param seu A Seurat object containing ADT data.
+#' @param x A string representing the name of the feature to plot on the x-axis.
+#' @param y A string representing the name of the feature to plot on the y-axis.
+#' @param color (Optional) A string representing the name of the feature to use for coloring the dots. Defaults to \code{NULL}.
+#' @param addvalue A numeric value added to each feature value before log transformation. Defaults to \code{1}.
+#' @param downsample A numeric value between 0 and 1 specifying the fraction of the data to randomly sample. Defaults to \code{0.2}.
+#' @param cols (Optional) A vector of colors to use for coloring the dots. Can be used for both continuous and categorical color scales. Defaults to \code{NULL}.
+#' @param add_noise A logical indicating whether to add noise (via jittering) to the data before plotting. Defaults to \code{TRUE}.
+#' @param size A numeric value specifying the size of the dots in the plot. Defaults to \code{0.8}.
+#' @param xpbuff A numeric value specifying the positive buffer for the x-axis limits. Defaults to \code{0.5}.
+#' @param ypbuff A numeric value specifying the positive buffer for the y-axis limits. Defaults to \code{0.5}.
+#' @param xnbuff A numeric value specifying the negative buffer for the x-axis limits. Defaults to \code{0}.
+#' @param ynbuff A numeric value specifying the negative buffer for the y-axis limits. Defaults to \code{0}.
+#' @param remove_negs A logical value indicating whether to remove points with negative values on the x or y axes after log transformation. Defaults to \code{TRUE}.
+#' 
+#' @return A ggplot object representing the dot plot.
+#' 
+#' @examples
+#' # Example usage
+#' adt_dotplot(seu = my_seurat_object, 
+#'             x = "ADT_feature1", 
+#'             y = "ADT_feature2", 
+#'             color = "ADT_feature3")
+#'
+#' @importFrom Seurat FetchData
+#' @importFrom ggplot2 ggplot aes geom_point theme_bw xlim ylim scale_color_gradient scale_color_manual xlab ylab guides guide_legend
+#' @export
+adt_dotplot <- function(seu, 
+                        x,
+                        y,
+                        color = NULL,
+                        addvalue = 1, 
+                        downsample = 0.2,
+                        cols = NULL,
+                        add_noise = TRUE,
+                        size = 0.8,
+                        xpbuff = 0.5,
+                        ypbuff = 0.5,
+                        xnbuff = 0,
+                        ynbuff = 0,
+                        remove_negs = TRUE
+                        ){
+  vars <- c(x, y, color)
+  df<-FetchData(seu, vars=vars, layer = "counts")
+  if(add_noise){
+    amount<-0.8
+    factor<-1
+    nbsize = 3
+    nbprob = 0.5
+    input<-rbinom(length(df[[vars[2]]]), size = nbsize, prob = nbprob)+df[[vars[2]]]
+    df$HTO_Y<-log10(jitter(input+addvalue, amount=amount, factor = factor))
+    input<-rbinom(length(df[[vars[1]]]), size = nbsize, prob = nbprob)+df[[vars[1]]]
+    df$HTO_X<-log10(jitter(input+addvalue, amount=amount, factor = factor))
+  } else {
+    df$HTO_Y<-log10(df[[vars[2]]]+addvalue)
+    df$HTO_X<-log10(df[[vars[2]]]+addvalue)
+  }
+  if(remove_negs){
+    df <- df[!(df$HTO_Y < 0),]
+    df <- df[!(df$HTO_X < 0),]
+  }
+  df$color <- df[[color]]
+  ymax = max(df$HTO_Y)+ ypbuff
+  xmax = max(df$HTO_X)+ xpbuff
+  ymin = min(df$HTO_Y)- ynbuff
+  xmin = min(df$HTO_X)- xnbuff
+  g <- ggplot(df, aes(x=HTO_X, y=HTO_Y, color=color))+
+    geom_point(size = size)+
+    xlim(xmin, xmax)+
+    ylim(ymin, ymax)+
+    theme_bw()+
+    xlab(x)+
+    ylab(y)+ 
+    guides(colour = guide_legend(override.aes = list(size=3)))
+
+  if(!is.null(cols)){
+    if(is.numeric(df$color)){
+      g+scale_color_gradient(cols)
+    } else {
+      g+scale_color_manual(values=cols)
+    }
+    
+  } else {
+    g
+  }
+}
+
+#' Generate a Panel of Dot Plots for AML Markers
+#'
+#' This function creates a panel of dot plots for Acute Myeloid Leukemia (AML) marker expression, using ADT data from a Seurat object. Each plot compares pairs of AML-related surface markers, and the plots are arranged in a grid. The color of the points can be customized based on a specified feature.
+#'
+#' @param seu A Seurat object containing ADT data.
+#' @param color A string representing the name of the feature to use for coloring the dots. Defaults to \code{"vmR_pred"}.
+#' @param cols A vector of colors to use for the coloring the dots. Can be used for both continuous and categorical color scales.
+#'
+#' @return A patchwork object representing a grid of dot plots.
+#'
+#' @examples
+#' # Example usage
+#' aml_panel(seu = my_seurat_object, color = "vmR_pred", cols = my_colors)
+#'
+#' @importFrom Seurat FetchData
+#' @importFrom ggplot2 NoLegend
+#' @importFrom patchwork +
+#' @export
+aml_panel <- function(seu, color = "vmR_pred", cols){
+  p1<-adt_dotplot(seu, "CD45", "CD34", color, cols = cols)+NoLegend()
+  p2<-adt_dotplot(seu, "CD117", "CD34", color, cols = cols)+NoLegend()
+  p3<-adt_dotplot(seu, "CD38", "HLA-DRA", color, cols = cols)+NoLegend()
+  p4<-adt_dotplot(seu, "CD33", "CD13", color, cols = cols)+NoLegend()
+  p5<-adt_dotplot(seu, "CD71", "CD15", color, cols = cols)+NoLegend()
+  p6<-adt_dotplot(seu, "CD14", "CD64", color, cols = cols)+NoLegend()
+  p7<-adt_dotplot(seu, "CD34", "CD123", color, cols = cols)+NoLegend()
+  p8<-adt_dotplot(seu, "CD11b", "CD11c", color, cols = cols)+NoLegend()
+  p9<-adt_dotplot(seu, "CD56", "CD34", color, cols = cols)+NoLegend()
+  p1+p2+p3+p4+p5+p6+p7+p8+p9
+}
+
+all_panel <- function(seu, color = "vmR_pred", cols){
+  p1<-adt_dotplot(seu, "CD45", "CD10", color, cols = cols)+NoLegend()
+  p2<-adt_dotplot(seu, "CD10", "CD19", color, cols = cols)+NoLegend()
+  p3<-adt_dotplot(seu, "CD34", "CD22", color, cols = cols)+NoLegend()
+  p4<-adt_dotplot(seu, "CD20", "CD38", color, cols = cols)+NoLegend()
+  #p5<-adt_dotplot(seu, "CD79a", "CD13", color, cols = cols)+NoLegend()
+  #p6<-adt_dotplot(seu, "CD58", "CD9", color, cols = cols)+NoLegend()
+  p5<-adt_dotplot(seu, "CD19", "CD33", color, cols = cols)+NoLegend()
+  #p8<-adt_dotplot(seu, "Lambda", "Kappa", color, cols = cols)+NoLegend()
+  p6<-adt_dotplot(seu, "CD3D", "CD7", color, cols = cols)+NoLegend()
+  p7<-adt_dotplot(seu, "CD4", "CD8A", color, cols = cols)+NoLegend()
+  p8<-adt_dotplot(seu, "CD56", "CD16", color, cols = cols)+NoLegend()
+  #p12<-adt_dotplot(seu, "CD5", "CD1a", color, cols = cols)+NoLegend()
+  p9<-adt_dotplot(seu, "CD34", "CD2", color, cols = cols)+NoLegend()
+  p10<-adt_dotplot(seu, "TCRab", "TCRVd2", color, cols = cols)+NoLegend()
+  #p22<-adt_dotplot(seu, "CD7", "CD1a", color, cols = cols)+NoLegend()
+  p11<-adt_dotplot(seu, "CD34", "CD5", color, cols = cols)+NoLegend()
+  p1+p2+p3+p4+p5+p6+p7+p8+p9+p10+p11
+}
